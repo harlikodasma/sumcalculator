4da5a678e96fd52dd961d8005dc942455b31ebe9
Harli Kodasma <harlikodasma@users.noreply.github.com>
2023-02-11 13:53:04 +0200

    Initial commit

---
+++ src/main/java/com/github/harlikodasma/sumcalculator/SumcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.harlikodasma.sumcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumcalculatorApplication.class, args);
+    }
+
+}
